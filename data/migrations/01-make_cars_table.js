8273f44cc987a2ad7ac6eac551986bc687fe63cf
Victor Tran <victor-likes-coding@tempestas.io>
2023-03-24 19:33:49 -0400

    write first migration, cars table schema

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,23 @@
+/**
+ * @param {import('knex').Knex} knex
+ */
 exports.up = function (knex) {
   // DO YOUR MAGIC
+  knex.schema.createTable('cars', (table) => {
+    table.increments();
+    table.string('vin', 17).unique().notNullable();
+    table.string('make', 128).notNullable();
+    table.string('model', 128).notNullable();
+    table.integer('mileage').notNullable();
+    table.string('title', 128);
+    table.string('transmission', 128);
+  });
 };
 
+/**
+ * @param {import('knex').Knex} knex
+ */
 exports.down = function (knex) {
   // DO YOUR MAGIC
+  knex.schema.dropTableIfExists('cars');
 };
