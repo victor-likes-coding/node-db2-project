737e2133d4e272bb0d8e7710610523a55fab3d86
Victor Tran <victor-likes-coding@tempestas.io>
2023-03-24 19:51:03 -0400

    import carRouter and use middleware/router

--- api/server.js
+++ api/server.js
@@ -1,7 +1,9 @@
-const express = require("express")
-
-const server = express()
+const express = require('express');
+const carRouter = require('./cars/cars-router');
+const server = express();
 
 // DO YOUR MAGIC
+server.use(express.json());
+server.use('/api/cars', carRouter);
 
-module.exports = server
+module.exports = server;
