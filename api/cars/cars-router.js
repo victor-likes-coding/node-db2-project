2b73bd13f2a018284ce5a9184b6cdedaa031083a
Victor Tran <victor-likes-coding@tempestas.io>
2023-03-24 19:50:00 -0400

    flesh out router / no routes yet

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,5 @@
-// DO YOUR MAGIC
+const express = require('express');
+const router = express.Router();
+
+const Cars = require('./cars-model');
+const { checkCarId, checkCarPayload, checkVinNumberValid, checkVinNumberUnique } = require('./cars-middleware');
