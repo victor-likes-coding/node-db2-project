3b396dcbcb1024a0b2f42efce1b93595cef328ca
Victor Tran <victor-likes-coding@tempestas.io>
2023-03-24 19:33:57 -0400

    write model helpers

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -1,11 +1,27 @@
-const getAll = () => {
-  // DO YOUR MAGIC
-}
+const /** @type {import('knex')} */ db = require('../../data/db-config');
 
-const getById = () => {
+exports.getAll = () => {
   // DO YOUR MAGIC
-}
+  return db('cars');
+};
 
-const create = () => {
+/**
+ *
+ * @param {int} id
+ * @returns {Promise<Car>}
+ */
+exports.getById = (id) => {
   // DO YOUR MAGIC
-}
+  return db('cars').where({ id }).first();
+};
+
+/**
+ * @param {{ vin: string, make: string, model: string, mileage: number, title?: string, transmission?: string }} car
+ */
+
+exports.create = (car) => {
+  // DO YOUR MAGIC
+  return db('cars')
+    .insert(car)
+    .then(([id]) => this.getById(id));
+};
